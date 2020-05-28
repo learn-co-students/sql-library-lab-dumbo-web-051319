337a9aff7eb70f20fa7d852fa5659286625dc6d2
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2020-05-28 01:22:58 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year 
+  FROM books 
+  WHERE series_id ='1';"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto
+  FROM characters
+  WHERE length(motto) = (SELECT max(length(motto)) FROM characters);"
 end
 
+# Searched "sql find the longest string"
+# Answer: https://stackoverflow.com/questions/21885267/how-to-find-longest-string-in-the-table-column-data
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT (*)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
@@ -16,7 +26,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT "
 end
 
 def select_character_names_and_number_of_books_they_are_in
